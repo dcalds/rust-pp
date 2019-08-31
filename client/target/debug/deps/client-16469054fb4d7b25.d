718d3a346abf25ba66f0e0e97386566c9b8d74a7
Danilo Caldas <danilo-caldas-0000@hotmail.com>
2019-08-31 13:57:25 -0300

    Projeto com C++ build tools

---
+++ client/target/debug/deps/client-16469054fb4d7b25.d
@@ -0,0 +1,5 @@
+C:\Users\Danilo\Desktop\rust-pp\client\target\debug\deps\client-16469054fb4d7b25.exe: src\main.rs
+
+C:\Users\Danilo\Desktop\rust-pp\client\target\debug\deps\client-16469054fb4d7b25.d: src\main.rs
+
+src\main.rs:
